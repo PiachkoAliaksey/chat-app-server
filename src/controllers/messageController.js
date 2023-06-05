55b15cd7c85accbc3cdfeeeefbb0e012d531e9bb
PiachkoAliaksey <alek17y@mail.ru>
2023-06-05 20:59:35 +0300

    add: message controllers

---
+++ src/controllers/messageController.js
@@ -0,0 +1,39 @@
+import messageModel from "../messageModel";
+
+export const addMessage = async (req, res, next) => {
+    try {
+        const { from, to, message } = req.body;
+        const data = await messageModel.create({
+            message: { text: message },
+            users: [from, to],
+            sender: from,
+
+        });
+        if (data) {
+            res.json({ msg: "sent successfully" });
+        }
+        res.json({ msg: "Failed to add message to database" });
+    } catch (err) {
+        next(err)
+    }
+}
+
+export const getAllMessage = async (req, res, next) => {
+    try {
+        const { from, to } = req.body;
+        const messages = await messageModel.find({
+            users: {
+                $all: [from, to]
+            }
+        }).sort({ updatedAt: 1 });
+        const projectMessages = messages.map((msg) => {
+            return {
+                fromSelf: msg.sender.toString() === from,
+                message: msg.message.text,
+            }
+        })
+        res.json(projectMessages)
+    } catch (err) {
+        next(err)
+    }
+}
