7ad7669a62a956ac04942919b2bee83235335122
PiachkoAliaksey <alek17y@mail.ru>
2023-06-10 20:14:25 +0300

    refactor: message controller

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -1,18 +1,19 @@
-import messageModel from "../messageModel";
+import messageModel from "../messageModel.js";
 
 export const addMessage = async (req, res, next) => {
     try {
-        const { from, to, message } = req.body;
+        const { from, to, message, title } = req.body;
         const data = await messageModel.create({
-            message: { text: message },
+            message: { textTitle:title, text: message },
             users: [from, to],
             sender: from,
 
         });
         if (data) {
             res.json({ msg: "sent successfully" });
+        }else{
+            res.json({ msg: "Failed to add message to database" });
         }
-        res.json({ msg: "Failed to add message to database" });
     } catch (err) {
         next(err)
     }
@@ -21,18 +22,47 @@ export const addMessage = async (req, res, next) => {
 export const getAllMessage = async (req, res, next) => {
     try {
         const { from, to } = req.body;
-        const messages = await messageModel.find({
-            users: {
-                $all: [from, to]
-            }
-        }).sort({ updatedAt: 1 });
-        const projectMessages = messages.map((msg) => {
-            return {
-                fromSelf: msg.sender.toString() === from,
-                message: msg.message.text,
-            }
-        })
-        res.json(projectMessages)
+        if(from===to){
+            const messages = await messageModel.find({
+                "$expr": {
+                    "$eq": [
+                      { "$arrayElemAt": ["$users", 0] },
+                      { "$arrayElemAt": ["$users", 1] }
+                    ]
+                  }
+            }).find({
+                users: {
+                    $all: [from, to]
+                }
+            }).sort({ updatedAt: 1 });
+            const projectMessages = messages.map((msg) => {
+                return {
+                    fromSelf: msg.sender.toString() === from,
+                    title:msg.message.textTitle,
+                    message: msg.message.text,
+                    created: msg.createdAt.toString().slice(16, 25)
+                }
+            })
+            res.json(projectMessages)
+            
+        }else{
+            const messages = await messageModel.find({
+                users: {
+                    $all: [from, to]
+                }
+            }).sort({ updatedAt: 1 });
+            
+            const projectMessages = messages.map((msg) => {
+                return {
+                    fromSelf: msg.sender.toString() === from,
+                    title:msg.message.textTitle,
+                    message: msg.message.text,
+                    created: msg.createdAt.toString().slice(16, 25)
+                }
+            })
+            res.json(projectMessages)
+        } 
+        
     } catch (err) {
         next(err)
     }
